4d323475f353cea7aa045db261340149cc47b2c6
Daniel Fernandes <daniel.dourado@gmail.com>
2018-04-23 14:56:28 +0000

    Primeiro commit

---
+++ scripts/rotater.gd
@@ -0,0 +1,15 @@
+extends StaticBody2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+export var rotation_velocity = 1
+
+func _ready():
+	set_process(true)
+	add_to_group(game.GROUP_PIPES)
+	pass
+	
+func _process(delta):
+	var rotation = self.get_rot()
+	self.set_rot(rotation+rotation_velocity*0.01)
