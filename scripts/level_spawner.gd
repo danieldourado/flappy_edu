fc8e333eebbeb20debb044bfacb35208e9b42689
Daniel Fernandes <daniel.dourado@gmail.com>
2018-05-21 15:18:01 +0000

    novo sprite para o botão de pause novo sprite para o botão de instrucoes a fase só é adicionada ao stage após o usuário clicar nas instruções

--- scripts/level_spawner.gd
+++ scripts/level_spawner.gd
@@ -1,6 +1,16 @@
 extends Node2D
 
+var bird = null
+var new_level = null
+
 func _ready():
+	set_process(true)
 	var level = load("res://fases/fase"+str(stage_manager.current_level)+".tscn")
-	var new_level = level.instance()
-	add_child(new_level)
+	new_level = level.instance()
+	bird = utils.get_main_node().get_node("bird")
+
+func _process(delta):
+	if bird.get_state() == bird.STATE_FLAPPING:
+		add_child(new_level)
+		set_process(false)
+
