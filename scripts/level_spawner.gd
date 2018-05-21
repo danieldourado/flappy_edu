87ed9076f84c845560422fbf1ba747453d755774
Daniel Fernandes <daniel.dourado@gmail.com>
2018-05-21 17:10:58 +0000

    consertado bug que percorria a fase quando não adicionada

--- scripts/level_spawner.gd
+++ scripts/level_spawner.gd
@@ -10,6 +10,7 @@ func _ready():
 	bird = utils.get_main_node().get_node("bird")
 
 func _process(delta):
+	set_pos(Vector2(bird.get_pos().x, get_pos().y))
 	if bird.get_state() == bird.STATE_FLAPPING:
 		add_child(new_level)
 		set_process(false)
