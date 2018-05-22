712eb94ec1102fa7fd63b9ceee3034b00bb29897
EC2 Default User <ec2-user@ip-172-31-26-184.us-east-2.compute.internal>
2018-05-22 16:59:09 +0000

    todas as moedas implementadas

--- scripts/level_spawner.gd
+++ scripts/level_spawner.gd
@@ -1,6 +1,17 @@
 extends Node2D
 
+var bird = null
+var new_level = null
+
 func _ready():
+	set_process(true)
 	var level = load("res://fases/fase"+str(stage_manager.current_level)+".tscn")
-	var new_level = level.instance()
-	add_child(new_level)
+	new_level = level.instance()
+	bird = utils.get_main_node().get_node("bird")
+
+func _process(delta):
+	set_pos(Vector2(bird.get_pos().x, get_pos().y))
+	if bird.get_state() == bird.STATE_FLAPPING:
+		add_child(new_level)
+		set_process(false)
+
